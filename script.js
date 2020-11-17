1e91444ef8632b3c0e1e5e7970ee84be06c15a88
WisamAlhroub <ateyyahs@gmail.com>
2020-11-17 10:48:08 +0200

    Challenge6

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function handlePositives(e) {
     let input = document.getElementById("challenge1").value;
 
@@ -52,6 +51,32 @@ function handleCapitalizeFirst(e) {
     }).join(' ');
     
     document.getElementById("output5").innerHTML = n;
+}
+
+function handleBubbleSort(e) {
+    let input1 = document.getElementById("challenge6-1").value;
+    let obj = JSON.parse(input1);
+
+    let bubbleSort = (inputArr) => {
+        let len = inputArr.length;
+        for (let i = 0; i < len; i++) {
+            for (let j = 0; j < len; j++) {
+                if (inputArr[j] > inputArr[j + 1]) {
+                    let tmp = inputArr[j];
+                    inputArr[j] = inputArr[j + 1];
+                    inputArr[j + 1] = tmp;
+                }
+            }
+        }
+        return inputArr;
+    };
+
+    if (!Array.isArray(obj)) result = "Please enter an array";
+    else{
+        result = bubbleSort(obj);
+    }
+    document.getElementById("output6").innerHTML = result;
+
 }
 
 function positives(arr) {
